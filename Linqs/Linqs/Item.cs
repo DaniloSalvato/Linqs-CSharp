beb3749fdeda4db351e967cd991bb64df1716c9b
Danilo Miguel <danilo_salvato@hotmail.com>
2023-01-15 18:39:01 -0300

    Fixacao

---
+++ Linqs/Linqs/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqs
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Categoria { get; set; }  
+        public bool Status { get; set; }
+        public decimal Valor { get; set; }
+        public List<Categoria> Categorias { get; set; }
+    }
+
+}
